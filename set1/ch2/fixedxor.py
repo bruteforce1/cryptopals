54c0b3160aeb202890a218869d6c3104c5cee9ad
mattj <jabroney182@gmail.com>
2016-05-30 00:38:58 -0400

    cleaned up and adhered to PEP8 standards

--- set1/ch2/fixedxor.py
+++ set1/ch2/fixedxor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Write a function that takes two equal-length buffers and produces their XOR combination.
+Write a function that takes two equal-length buffers and produces their
+    XOR combination.
 
 If your function works properly, then when you feed it the string:
 
@@ -25,11 +26,15 @@ def xorbinarystrings(str1, str2):
     if len(str1) != len(str2):
         print('XOR strings must be same length')
         return ''
-    #print(bytes("".join(chr(x ^ y) for x, y in zip(binascii.unhexlify(str1), binascii.unhexlify(str2))), 'utf-8'))
-    return binascii.hexlify(bytes("".join(chr(x ^ y) for x, y in zip(binascii.unhexlify(str1), binascii.unhexlify(str2))), 'utf-8')).decode('utf-8')
+    bstr1 = binascii.unhexlify(str1)
+    bstr2 = binascii.unhexlify(str2)
+    return binascii.hexlify(
+        ''.join(chr(x ^ y) for x, y in zip(bstr1,bstr2)
+        ).encode('utf-8')).decode('utf-8')
 
 def main():
-    ret = xorbinarystrings('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
+    ret = xorbinarystrings('1c0111001f010100061a024b53535009181c', 
+                           '686974207468652062756c6c277320657965')
     print(ret)
     if ret == '746865206b696420646f6e277420706c6179':
         print('It worked!')
@@ -37,6 +42,9 @@ def main():
     return -1
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Tests XOR\'ing a fixed hex value against another fixed string.')
+    parser = argparse.ArgumentParser(
+        description='Tests XOR\'ing a fixed hex value against another fixed \
+        string.'
+        )
     args = parser.parse_args()
     sys.exit(main())
