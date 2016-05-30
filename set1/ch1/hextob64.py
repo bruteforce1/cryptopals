6eef87979a34e3c9117592a5647e12f4cd8fa3fb
mattj <jabroney182@gmail.com>
2016-05-29 23:30:15 -0400

    Adhering to PEP standards

--- set1/ch1/hextob64.py
+++ set1/ch1/hextob64.py
@@ -15,14 +15,16 @@ import base64
 import sys
 
 def hextobase64(hexstr):
-    return base64.b64encode(bytes(bytes.fromhex(hexstr).decode('utf-8'),'utf-8')).decode('utf-8')
+    return base64.b64encode(
+        bytes(bytes.fromhex(hexstr).decode('utf-8'),'utf-8')).decode('utf-8')
 
 def main(hexstring):
     print(hextobase64(hexstring))
     return 0
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert a hex string to base64.')
+    parser = argparse.ArgumentParser(
+        description='Convert a hex string to base64.')
     parser.add_argument('hex', help='hex string')
     args = parser.parse_args()
     sys.exit(main(args.hex))
