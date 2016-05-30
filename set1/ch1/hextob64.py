63b4b1761096b6ef34e7bbf80f9905ebc29b336f
mattj <jabroney182@gmail.com>
2016-05-30 00:20:11 -0400

    Adhering to PEP8 standards and cleaning up python 3 bytes to str conversion sloppiness

--- set1/ch1/hextob64.py
+++ set1/ch1/hextob64.py
@@ -3,7 +3,8 @@
 """
  The string:
 
-49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f75\
+73206d757368726f6f6d'
 
 Should produce:
 
@@ -12,17 +13,17 @@ SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 
 import argparse
 import base64
+import binascii
 import sys
 
-def hextobase64(hexstr):
-    return base64.b64encode(
-        bytes(bytes.fromhex(hexstr).decode('utf-8'),'utf-8')).decode('utf-8')
+def hex_to_base64(hexstr):
+    return base64.b64encode(bytes.fromhex(hexstr)).decode('utf-8')
 
 def main(hexstring):
-    print(hextobase64(hexstring))
+    print(hex_to_base64(hexstring))
     return 0
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Convert a hex string to base64.')
     parser.add_argument('hex', help='hex string')
