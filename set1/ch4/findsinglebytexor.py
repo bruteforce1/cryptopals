e9ce5d5b6318a9dac43327bfe7ab625ec379d5ec
mattj <jabroney182@gmail.com>
2016-05-30 02:11:32 -0400

    first attempt at find single key xor in python 3

---
+++ set1/ch4/findsinglebytexor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+One of the 60-character strings in 4.txt has been encrypted by 
+    single-character XOR.
+
+Find it.
+
+(Your code from #3 should help.)
+
+"""
+
+import argparse
+import base64
+import string
+import sys
+
+sys.path.insert(0, '../ch3')
+
+from singlebytexor import find_key
+
+def display_ans(filename):
+    score = 0
+    keys = ('', '', '', '')
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            linekeys = find_key(line)
+            if linekeys[0] is not '' and score < linekeys[2]:
+                score = linekeys[2]
+                keys = (line, linekeys[0], linekeys[1], linekeys[2])
+    return keys
+
+def main():
+    ret = display_ans('./4.txt')
+    if ret[0]:
+        print('It worked!  Key list:\nLine: ' + ret[0] 
+            + '\nKey: ' + ret[1] 
+            + '\nPT: ' + ret[2]
+            )
+        return 0
+    print("Nothing found.")
+    return -1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Searches through list of encrypted data to find string \
+        encrypted by repeating key xor.'
+        )
+    args = parser.parse_args()
+    sys.exit(main())
