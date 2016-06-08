070ee9da5a386202db9ef053d90b6301d7ff0875
mattj <jabroney182@gmail.com>
2016-06-08 01:33:06 -0400

    Submitting AES Mode Detection in Python 3.

--- set2/ch11/detect_ecb_cbc.py
+++ set2/ch11/detect_ecb_cbc.py
@@ -14,7 +14,7 @@ The function should look like:
 encryption_oracle(your-input)
 => [MEANINGLESS JIBBER JABBER]
 
-Under the hood, have the function append 5-10 bytes (count choseni
+Under the hood, have the function append 5-10 bytes (count chosen
  randomly) before the plaintext and 5-10 bytes after the plaintext.
 
 Now, have the function choose to encrypt under ECB 1/2 the time, and
@@ -30,21 +30,55 @@ import argparse
 import random
 import string
 import sys
+sys.path.insert(0, '../../utils')
+from cpset2 import aes_ecb, aes_cbc, test_aes_ecb
 
 random.seed()
 
-def gen_random_key(block=16):
+def gen_random_bytes(block=16):
     if not 1 <= block <= 32:
         return b''
-    rands = []
-    for x in range(1,block):
-        rands.append(random.randint(0,255))
-    return bytearray(rands)
+    return bytes(random.randint(0,255) for _ in range(block))
 
 def encryption_oracle(text):
-    bef = gen_random_key(random.randint(5,10))
-    af = gen_random_key(random.randint(5,10))
-    pt = bef + text + af
-    print(pt)
+    if type(text).__name__ == 'str':
+        t = text.encode('utf-8')
+    elif type(message).__name__ == 'bytes':
+        t = text
+    else:
+        return b''
 
-encryption_oracle(b'test')
+    key = gen_random_bytes()
+    bef = gen_random_bytes(random.randint(5,10))
+    af = gen_random_bytes(random.randint(5,10))
+    pt = bef + t + af
+    if random.randint(0,1):
+        iv = gen_random_bytes()
+        ret = aes_cbc(pt, key, iv)
+        encmode = 0
+    else:
+        ret = aes_ecb(pt, key)
+        encmode = 1
+    guess = test_aes_ecb(ret)
+    print('AES Mode: {0}; Guess: {1}'.format(['CBC','ECB'][encmode],
+        ['CBC','ECB'][guess]))
+    if encmode == guess:
+        return 1
+    return 0
+
+def main():
+    ans = encryption_oracle('a' * 1000)
+    if ans:
+        print('AES Mode Detected!')
+        return 0
+    print('Wrong Guess.')
+    return -1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Uses an encryption oracle to encrypt data with AES in \
+        either ECB or CBC mode.  Then we guess which method is used.'
+        )
+    args = parser.parse_args()
+
+    sys.exit(main())
