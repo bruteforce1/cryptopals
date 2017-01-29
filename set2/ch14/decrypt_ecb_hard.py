a7ab59ef522243719d88100cd6519ebc229a33ba
bruteforce1 <jabroney182@gmail.com>
2017-01-28 23:37:57 -0500

    Finished set2 challenge 14!

--- set2/ch14/decrypt_ecb_hard.py
+++ set2/ch14/decrypt_ecb_hard.py
@@ -23,22 +23,13 @@ Think "STIMULUS" and "RESPONSE".
 
 import argparse
 import base64
-import math
-import os
 import random
-import string
 import sys
-sys.path.insert(0, './utils')
-from cpset2 import aes_ecb, gen_random_bytes, test_aes_ecb
+from utils.cpset2 import aes_ecb, gen_random_bytes, test_aes_ecb
 
 random.seed(1)
 GLOBAL_KEY = gen_random_bytes(16)
 
-def is_oracle_ecb(block):
-    enc = encryption_oracle('B' * block * 10)
-    if test_aes_ecb(enc,block):
-        return True
-    return False
 
 def convert_to_bytes(text):
     if type(text).__name__ == 'str':
@@ -49,27 +40,29 @@ def convert_to_bytes(text):
         raise TypeError('Bad type passed to encryption_oracle')
     return t
 
+
 def decrypt_ecb(block):
     ans = b''
     mult = 0
     ora, enc = find_first_oracle(block)
-    ctlen = len(enc)
+    ct_len = len(enc) + block
 
-    while len(ans) < ctlen:
+    while len(ans) < ct_len:
         if len(ans) % block == 0:
             mult += 1
-        pad = b'B' * (block) + b'A' * (block - (len(ans)%block + 1))
+        pad = b'B' * block + b'A' * (block - (len(ans)%block + 1))
         oracle = b''
         while ora not in oracle:
-            oracle = encryption_oracle(pad)
-        print(ora)
-        print(oracle)
+            oracle = base64.b64decode(encryption_oracle(pad))
         oracle = oracle.split(ora)[1]
         found = 0
-        for test in range(0,255):
+        for test in range(0, 255):
             te = pad + ans + bytes([test])
-            enc = encryption_oracle(te)
-            if base64.b64decode(enc)[:block*mult] == base64.b64decode(oracle)[:block*mult]:
+            enc = b''
+            while ora not in enc:
+                enc = base64.b64decode(encryption_oracle(te))
+            enc = enc.split(ora)[1]
+            if enc[:block*mult] == oracle[:block*mult]:
                 ans += bytes([test])
                 found = 1
                 break
@@ -81,14 +74,54 @@ def decrypt_ecb(block):
         print('Decrypted text: ')
         print(ans)
         return ''
-    
     return ans[:-pad].decode('utf-8')
+
+
+def encryption_oracle(text):
+    ora_prefix = gen_random_bytes(random.randrange(0, 64))
+    crypt = 'Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg'
+    crypt += 'aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq'
+    crypt += 'dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg'
+    crypt += 'YnkK'
+    return aes_ecb(convert_to_bytes(ora_prefix) + convert_to_bytes(text)
+                   + base64.b64decode(crypt),
+                   convert_to_bytes(GLOBAL_KEY), 1)
+
+
+def find_first_oracle(block):
+    count = 0
+    while count < block:
+        found = False
+        while not found:
+            enc = encryption_oracle(b'B' * (2*block))
+            if test_aes_ecb(enc):
+                found = True
+        blocks = []
+        crypt = base64.b64decode(enc)
+        for x in range(block, len(crypt), block):
+            blocks.append(crypt[x-block:x])
+        # blocks.sort()
+        for y in range(1, len(blocks)-1):
+            if blocks[y-1] == blocks[y]:
+                return blocks[y], b''.join(x for x in blocks[y+1:])
+        count += 1
+    return ''
+
+
+def find_max_block_size(test):
+    l = 0
+    for i in range(1, 256):
+        tl = len(encryption_oracle(test))
+        if l < tl:
+            l = tl
+    return l
+
 
 def get_oracle_block_size():
     l = 0
     resize = 0
     cnt = 0
-    for i in range(1,100):
+    for i in range(1, 100):
         test = b'A' * i
         tl = find_max_block_size(test)
         if l == 0:
@@ -104,32 +137,12 @@ def get_oracle_block_size():
             return cnt
     return -1
 
-def find_max_block_size(test):
-    l = 0
-    for i in range(1,256):
-        tl = len(encryption_oracle(test))
-        if l < tl:
-            l = tl
-    return l
 
-def find_first_oracle(block):
-    found = False
-    while not found:
-        enc = encryption_oracle(b'B' * (2*block))
-        if test_aes_ecb(enc):
-            found = True
-   
-    blocks = []
-    crypt = base64.b64decode(enc)
-    for x in range(block,len(crypt),block):
-        blocks.append(crypt[x-block:x])
-    blocks.sort()
-    for y in range(1,len(blocks)):
-        if blocks[y-1] == blocks[y]:
-            print(blocks[y])
-            print(b''.join(x for x in blocks[y+1:]))
-            return blocks[y], b''.join(x for x in blocks[y+1:])
-    return ''
+def is_oracle_ecb(block):
+    enc = encryption_oracle('B' * block * 10)
+    if test_aes_ecb(enc, block):
+        return True
+    return False
 
 
 def manage_decrypt_aes_ecb():
@@ -140,15 +153,6 @@ def manage_decrypt_aes_ecb():
             return decrypt_ecb(bs)
     return ''
 
-def encryption_oracle(text):
-    oraprefix = gen_random_bytes(random.randrange(0,64))
-    crypt = 'Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg'
-    crypt += 'aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq'
-    crypt += 'dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg'
-    crypt += 'YnkK'
-    return aes_ecb(convert_to_bytes(oraprefix) + convert_to_bytes(text)
-                   + base64.b64decode(crypt), 
-                   convert_to_bytes(GLOBAL_KEY),1)
 
 def main():
     ans = manage_decrypt_aes_ecb()
@@ -161,7 +165,7 @@ def main():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Uses an oracle to decrypt AES in ECB mode, one byte at \
-        a time.  This is the simple approach.'
+        a time.  This is the hard approach.'
         )
     args = parser.parse_args()
 
